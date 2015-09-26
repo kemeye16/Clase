d79b846a936b70aa4604524347b78a497a8d6078
kemeye16 <kemeye.16@gmail.com>
2015-09-26 16:39:53 -0600

    Cambios Finales

--- ResolverEcuacionCuadratica.Test/UnitTest1.cs
+++ ResolverEcuacionCuadratica.Test/UnitTest1.cs
@@ -36,4 +36,12 @@ asegurese de que los valores reales son iguales a los esperados
 
         }
     }
+    /*
+    tarea Moral:
+    cree pruebas unitarias para los siguientes casos:
+    -Pruebas correctas para resultados reales con muchos decimales,
+    de modo que la coincidencia NO sea exacta y se apruebe
+    cuando hay coincidan al menos en el sexto decimal
+    -pruebas correctas para cuando el discriminante 
+    */
 }
